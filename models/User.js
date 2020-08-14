bf2c8c9769003799c5a50dcc800755092ce2e324
BingShawLim <shaw.a.muvee@gmail.com>
2020-08-14 18:04:31 -0500

    setup and save login session in mongodb

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const userCollection = require('../db').collection('user')
+const bcrypt = require('bcryptjs')
+const userCollection = require('../db').db().collection('user')
 const validator = require('validator')
 
 let User = function(data) {
@@ -11,7 +12,6 @@ User.prototype.cleanUp = function () {
     if (typeof(username) !== "string") {username = ""}
     if (typeof(email) !== "string") {email = ""}
     if (typeof(password) !== "string") {password = ""}
-
     //get rid of bogus properties
     this.data = {
         username: username.trim().toLowerCase(),
@@ -29,17 +29,38 @@ User.prototype.validate = function() {
     if(username.length >0 && username.length < 3){this.errors.push("Username must be at least 3 characters.")}
     if(username.length >30){this.errors.push("Username can't exceed 30 characters.")}
     if(password.length >0 && password.length < 6){this.errors.push("Password must be at least 6 characters.")}
-    if(password.length >80){this.errors.push("Username can't exceed 80 characters.")}
+    if(password.length >50){this.errors.push("Username can't exceed 50 characters.")}
+}
+
+User.prototype.login = function() {
+   return new Promise((resolve, reject)=>{
+    this.cleanUp()
+    userCollection.findOne({username: this.data.username}).then((attemptedUser)=>{
+        if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+            resolve("yes!")
+        } else {
+            reject("wrong!")
+        }
+    })
+    .catch(()=> {
+        reject("please try again later")
+    })
+})
 }
 
 User.prototype.register = function() {
     this.cleanUp()
     // validate user data
     this.validate()
+
     // no errors && save to database
     if (!this.errors.length) {
+        // hash the password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         userCollection.insertOne(this.data)
     }
 
 }
+
 module.exports = User
--- router.js
+++ router.js
@@ -5,5 +5,6 @@ const userController = require('./controllers/userController.js')
 router.get('/', userController.home)
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports = router
