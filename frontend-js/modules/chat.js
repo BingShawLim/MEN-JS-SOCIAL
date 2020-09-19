e537b9c1bca9c15e5a04bf40a62824e37ed68a23
BingShawLim <shaw.a.muvee@gmail.com>
2020-09-18 22:04:03 -0500

    added socket.io, start build up chat function.

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -4,13 +4,25 @@ export default class Chat {
         this.chatWrapper = document.querySelector("#chat-wrapper")
         this.openIcon = document.querySelector(".header-chat-icon")
         this.injectHTML()
+        this.chatField = document.querySelector("#chatField")
+        this.chatForm = document.querySelector("#chatForm")
         this.closeIcon = document.querySelector(".chat-title-bar-close")
         this.events()
     }
 
     events() {
+        this.chatForm.addEventListener("submit", (e) => {
+            e.preventDefault()
+            this.sendMessageToServer()
+        })
         this.openIcon.addEventListener("click", () => this.showChat())
         this.closeIcon.addEventListener("click", () => this.hideChat())
+    }
+
+    sendMessageToServer() {
+        this.socket.emit('chatMessageFromBrowser', { message: this.chatField.value })
+        this.chatField.value = ''
+        this.chatField.focus()
     }
 
     showChat() {
@@ -22,7 +34,10 @@ export default class Chat {
     }
 
     openConnection() {
-       this.socket = io()
+        this.socket = io()
+        this.socket.on('chatMessageFromServer', function (data) {
+            
+        })
     }
 
     hideChat() {
